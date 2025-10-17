f11044ebf988e6ec93d491d612b972e058cc6b3c
shubhamparmar <shubhamparmar@thoughtwin.com>
2025-10-17 14:21:25 +0530

    publishing main branch

--- api/auth/v1.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from services.auth.jwt.routes import router as jwt_routes
-
-router = APIRouter()
-router.include_router(jwt_routes)
--- api/user/v1.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from services.users.routes import router as user_routes
-
-router = APIRouter()
-router.include_router(user_routes)
